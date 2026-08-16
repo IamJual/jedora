21bb8550a684c0d387f23c4cbc1401dfab0ec9b0
Jual <jual.inbox.dev@outlook.com>
2026-08-16 16:52:19 +0300

    fgdashtsdfg

--- install.sh
+++ install.sh
@@ -1,59 +1,47 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-set -e
+# Create log directory
+if [ ! -d logs ]; then
+    mkdir logs
+fi
 
-echo "=== Jual's Fedora + Hyprland install script ==="
+LOG="logs/install-script-$(date +%d-%H%M%S).log"
+exec > >(tee -a "$LOG") 2>&1
 
-echo
-echo "=== Updating Fedora ==="
-sudo dnf upgrade --refresh -y
+if ! command -v whiptail &> /dev/null; then
+    sudo dnf install -y newt
+fi
 
-echo
-echo "=== Installing Fedora foundation ==="
-sudo dnf install -y \
-    @base-x \
-    @core \
-    @fonts \
-    @hardware-support \
-    @multimedia \
-    @networkmanager-submodules \
-    @printing
+clear
 
-echo
-echo "=== Enabling Hyprland repository ==="
-sudo dnf copr enable -y lionheartp/Hyprland
+export NEWT_COLORS='
+    root=white,black
+    border=white,black
+    window=white,black
+    shadow=black,black
+    title=yellow,black
+    button=black,lightgray
+    actbutton=black,cyan
+    compactbutton=white,black
+    textbox=white,black
+    acttextbox=white,black
+    entry=white,black
+    label=white,black
+    listbox=white,black
+    actlistbox=black,cyan
+    checkbox=white,black
+    actcheckbox=black,cyan
+'
 
-echo
-echo "=== Installing Hyprland desktop ==="
-sudo dnf install -y \
-    hyprland \
-    xdg-desktop-portal \
-    xdg-desktop-portal-hyprland \
-    xorg-x11-server-Xwayland \
-    sddm \
-    rofi \
-    kitty \
-    waybar \
-    hyprpaper \
-    thunar \
-    hyprpolkitagent
 
-echo
-echo "=== Installing useful extras ==="
-sudo dnf install -y \
-    btop \
-    fastfetch \
-    swaylock \
-    swayidle \
-    playerctl \
-    brightnessctl \
-    wl-clipboard
+# Welcome prompt
+whiptail --title "Welcome!" --msgbox "Welcome to the Jedora install sciprt. This is made to help you install fedora in various ways. Make sure you know waht you are doing before continuing wiht the instalation. I am not responsible for your mistakes!" 9 80
 
-echo
-echo "=== Configuring graphical boot ==="
-sudo systemctl enable sddm
-sudo systemctl set-default graphical.target
 
-echo
-echo "=== Setup complete ==="
-echo "Reboot your computer to start SDDM and Hyprland."
+if ! (whiptail --title "Proceed with Installation?" --yesno "TWould you like to proceed?" 7 50); then
+    echo Exiting...
+    exit 1
+fi
+
+# Fedora foundation
+
